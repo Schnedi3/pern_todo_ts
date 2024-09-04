fa383cfadb3486916f1cb4b7841aaf18f2ee4427
Schnedi3 <schnedi3@gmail.com>
2024-09-04 18:59:30 +0200

    Remove "add icon"

--- frontend/src/components/TaskForm.tsx
+++ frontend/src/components/TaskForm.tsx
@@ -3,20 +3,18 @@ import "../css/taskform.css";
 import { useTaskContext } from "../context/useTaskContext";
 
 export const TaskForm = () => {
-  const { newTask, handleOnSubmit, handleChange, addTask } = useTaskContext();
+  const { newTask, handleOnSubmit, handleChange } = useTaskContext();
 
   return (
     <form className="task_form" onSubmit={handleOnSubmit}>
       <h1>todo</h1>
-      <div className="input__container">
-        <input
-          placeholder="Add a new task"
-          onChange={handleChange}
-          value={newTask}
-          autoFocus
-        />
-        <p onClick={addTask}>🡒</p>
-      </div>
+      <input
+        className="input"
+        placeholder="Add a new task"
+        onChange={handleChange}
+        value={newTask}
+        autoFocus
+      />
     </form>
   );
 };
